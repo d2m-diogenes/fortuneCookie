8dcaa892bb48f7e539d9ed941bcc00fec569a7a3
Diogenes <diogenes.junior@gmail.com>
2022-03-31 23:04:20 -0300

    Finalizacao

--- App.js
+++ App.js
@@ -11,30 +11,45 @@ class App extends Component {
   constructor(props){
     super(props)
     this.state={
-      frase:[
-        'A vida trará coisas boas se tiver paciência.',
-        'Demonstre amor e alegria em todas as oportunidades e verá que a paz nasce dentro de si.',
-        'Não compense na ira o que lhe falta na razão.',
-        'Defeitos e virtudes são apenas dois lados da mesma moeda.',
-        'A maior de todas as torres começa no solo.',
-        'Não há que ser forte. Há que ser flexível.',
-        'Todos os dias organiza os seus cabelos, por que não faz o mesmo com o coração?',
-        'Há três coisas que jamais voltam; a flecha lançada, a palavra dita e a oportunidade perdida.',
-        'A juventude não é uma época da vida, é um estado de espírito.'
-      ]
+      imagem: require('./assets/img/biscoito.png'),
+      textoFrase: '',
     }
+
+    this.click = this.click.bind(this)
+
+    this.frases = [
+      'A vida trará coisas boas se tiver paciência.',
+      'Demonstre amor e alegria em todas as oportunidades e verá que a paz nasce dentro de si.',
+      'Não compense na ira o que lhe falta na razão.',
+      'Defeitos e virtudes são apenas dois lados da mesma moeda.',
+      'A maior de todas as torres começa no solo.',
+      'Não há que ser forte. Há que ser flexível.',
+      'Todos os dias organiza os seus cabelos, por que não faz o mesmo com o coração?',
+      'Há três coisas que jamais voltam; a flecha lançada, a palavra dita e a oportunidade perdida.',
+      'A juventude não é uma época da vida, é um estado de espírito.'
+    ]
+
+  }
+
+  click(){
+    let numero = Math.floor(Math.random() * this.frases.length)
+
+    this.setState({
+      textoFrase: this.frases[numero], 
+      imagem:require('./assets/img/biscoitoAberto.png')
+    })
   }
 
   render(){
     return (
       <View style={styles.container}>
-        <TouchableOpacity>
+        <TouchableOpacity onPress={this.click}>
           <Image
-            source={require('./assets/img/biscoito.png')}
+            source={this.state.imagem}
             style={styles.img}
           />
         </TouchableOpacity>
-        <Text style={styles.texto}>Teste</Text>
+        <Text style={styles.texto}>{this.state.textoFrase}</Text>
       </View>
     );
   }
@@ -49,11 +64,12 @@ const styles=StyleSheet.create({
     justifyContent:'center'
   },
   img:{
-
+    width: 300,
+    height: 300,
   },
   texto:{
     fontSize:30,
-    color:'blue',
-    textAlign:'center'
+    color:'#E45826',
+    textAlign:'center'    
   }  
 })
